6d6397c9acd25565b378752d302adafcc5782a60
Josoel <cvl.jv1@gmail.com>
2022-01-21 06:23:38 -0300

    Update contador com classe

--- excercicios/components/Contador.jsx
+++ excercicios/components/Contador.jsx
@@ -1,31 +1,41 @@
 import { Component } from "react";
 
 export default class Contador extends Component {
-    state ={ 
-        numero: this.props.valorInicial ?? 0
+    state = {
+        numero: this.props.valorInicial ?? 0,
+        passo: this.props.passo ?? 1
     }
     inc() {
-        this.setState({ numero: this.state.numero + (this.props.passo ?? 1) })
+        this.setState({ numero: this.state.numero + this.state.passo })
     }
     dec() {
-        this.setState({ numero: this.state.numero - (this.props.passo ?? 1) })
+        this.setState({ numero: this.state.numero - this.state.passo })
+    }
+    renderform() {
+        return (
+            <div>
+                <input type="number" min={1} max={1000} value={this.state.passo} onChange={e => this.setState({ passo: +e.target.value })} />
+                <button onClick={() => this.inc()}>+</button>
+                <button onClick={() => this.dec()}>-</button>
+            </div>
+        )
     }
 
     render() {
         return (
-            <div style={{display:"flex",
-            justifyContent:"center",
-            flexDirection:"column",
-            margin:"10px"}}>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                margin: "10px"
+            }}>
                 <h1>Contador</h1>
-                <h2 style={{background:"#222",
-                width:"100px",
-                textAlign:"center",
-            }}>{this.state.numero}</h2>
-                <div>
-                    <button onClick={() => this.inc()}>+</button>
-                    <button onClick={() => this.dec()}>-</button>
-                </div>
+                <h2 style={{
+                    background: "#222",
+                    width: "100px",
+                    textAlign: "center",
+                }}>{this.state.numero}</h2>
+                {this.renderform()}
             </div>
         )
     }
